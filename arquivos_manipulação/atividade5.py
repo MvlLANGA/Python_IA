cff46cf476cd46768109d40ab0f9a68f3b4e3328
Marcos Vinicius <langa.langa1984@gmail.com>
2025-06-04 16:55:21 -0300

    Trabalhando com arquivos

--- arquivos_manipulação/atividade5.py
+++ arquivos_manipulação/atividade5.py
@@ -1,10 +1,2 @@
 with open("dados.csv") as entrada:
-    linhas =  entrada.readlines()
-    maiores = []
-    for linha in linhas:
-        nome, idade = linha.strip().split(",")
-        if idade >= 18:
-            maiores.append(linha)
-with open("dados_maiores.csv", "w") as saida:
-    saida.writelines(maiores)
-    print("Arquivo 'dados_maiores' criado com sucesso!!")
+    
